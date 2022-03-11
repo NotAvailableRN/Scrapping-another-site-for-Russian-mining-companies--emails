c71bdee3ff9805a6d13e532dda514082c3ca473d
NotAvailableRN <100932870+NotAvailableRN@users.noreply.github.com>
2022-03-11 10:14:03 +0600

    Update Scrapper 3000.py

--- Scrapper 3000.py
+++ Scrapper 3000.py
@@ -1,4 +1,3 @@
-import bs4
 import requests
 import json
 import urllib
@@ -6,21 +5,21 @@ import csv
 
 def get_data(url):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.121 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.121 Safari/537.36" #header was taken directly from site
     }
     all_data = []
-    i = 1
-    for item in range(1, 84):
-        req = requests.get(url + f"{item}", headers)
-        json_response = req.json()
-        for i in range(0, 10):
+    y = 1
+    for item in range(1, 84):#info is downloaded via JSON file - total number of pages that we are looking for on site is 84
+        req = requests.get(url + f"{item}", headers) 
+        json_response = req.json() 
+        for i in range(0, 10): #each JSON reply has 10 values with info
             try:
-                email = (json_response["Page"][i]["Email"])
+                email = (json_response["Page"][i]["Email"]) #iterating through them
             except:
                 email = ("No email")
             all_data.append(email)
-            print(i)
-            i+=1
+            print(y) #just to see how many entries we have
+            y+=1
 
     with open(file='data.csv', mode='w', encoding="utf-8", newline='') as file:
         writer = csv.writer(file, delimiter='\t')
@@ -28,4 +27,4 @@ def get_data(url):
         for data in all_data:
             writer.writerow(data)
 
-get_data("https://whoiswho.dp.ru/api/Company/ListForSpecialization?specializationId=8106&pageSize=12&pageNumber=")
+get_data("https://whoiswho.dp.ru/api/Company/ListForSpecialization?specializationId=8106&pageSize=12&pageNumber=") #the site that we use
